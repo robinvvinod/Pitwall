6fd15094888fcbdb514f6672f0c15e02d6b0a350
robinvvinod <robinvvinod@users.noreply.github.com>
2023-04-13 19:27:06 +0800

    Added skeleton processing of timingData & timingAppData jsonStreams

--- livedata_stream.py
+++ livedata_stream.py
@@ -1,21 +1,18 @@
-import json
+import ujson
+
 
 def process_timing_app_data(msg):
-    
     # Seperate the timestamp from the JSON data
-    timestamp = msg[:12] # HH:MM:SS.MLS
-    receivedData = json.loads(msg[12:])["Lines"]
-    
+    timestamp = msg[:12]  # HH:MM:SS.MLS
+    receivedData = ujson.loads(msg[12:])["Lines"]
+
     # receivedData may contain information for more than 1 driver, where driver no. is the key
     for driver in receivedData:
-        
         # "in" checks for <class 'dict'> is O(1)
         if "Stints" in receivedData[driver]:
             for curStint in receivedData[driver]["Stints"]:
-
                 # Sometimes curStint is a list due to poor formatting in the livedata. We are only interested in curStint as the stint number of the car.
                 if isinstance(curStint, str):
-
                     if "TotalLaps" in receivedData[driver]["Stints"][curStint]:
                         # broadcast totalLaps done in tyres of current stint (incl in other sessions)
                         pass
@@ -31,10 +28,40 @@ def process_timing_app_data(msg):
                         pass
 
 
-fileH = open("jsonStreams/TimingAppData.jsonStream", "r", encoding="utf-8-sig")
+def process_timing_data(msg):
+    # Seperate the timestamp from the JSON data
+    timestamp = msg[:12]  # HH:MM:SS.MLS
+    receivedData = ujson.loads(msg[12:])["Lines"]
+
+    # receivedData may contain information for more than 1 driver, where driver no. is the key
+    for driver in receivedData:
+        if "GapToLeader" in receivedData[driver]:
+            # broadcast gap to leader
+            pass
+
+        if "IntervalToPositionAhead" in receivedData[driver]:
+            # broadcast gap to position ahead
+            pass
+
+        if "Sectors" in receivedData[driver]:
+            for indvSector in receivedData[driver]["Sectors"]:
+                # Sometimes indvSector is a list due to poor formatting in the livedate.
+                if isinstance(indvSector, str):
+                    # Sector number = indvSector
+                    if "Value" in receivedData[driver]["Sectors"][indvSector]:
+                        # sectorTime = receivedData[driver]["Sectors"][indvSector]["Value"]
+                        # broadcast
+                        pass
+
+        if "Speeds" in receivedData[driver]:
+            for indvSpeed in receivedData[driver]["Speeds"]:
+                if isinstance(indvSpeed, str):
+                    # whichSector = int(indvSpeed) + 1
+                    # speed = receivedData[driver]["Speeds"][indvSpeed]["Value"]
+                    # broadcast
+                    pass
+
+
+fileH = open("jsonStreams/TimingData.jsonStream", "r", encoding="utf-8-sig")
 for line in fileH:
-   process_timing_app_data(line)
-   
-
-    
-
+    process_timing_data(line)
