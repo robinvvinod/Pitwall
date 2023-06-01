5ba6eaed56749f7b3953022910f9308030d7a29f
robinvvinod <robinvvinod@users.noreply.github.com>
2023-06-01 20:46:34 +0800

    Added working rotating camera to lap comparison view

--- pitwall-ios/pitwall-ios/LapComparisonView.swift
+++ pitwall-ios/pitwall-ios/LapComparisonView.swift
@@ -7,29 +7,13 @@
 
 import SwiftUI
 import RealityKit
-
-struct Camera {
-    // Stores the x, y and z angles for the camera
-    var x: Float = 0
-    var y: Float = 0.5
-    var z: Float = 3
-}
-
-struct Car {
-    // Stores the x, y coords of cars
-    var x: Float = 0
-    var y: Float = 0
-}
-
-struct Cars {
-    var car1 = Car()
-    var car2 = Car()
-}
+import Combine
 
 struct LapComparisonView: View {
 
     @State private var cars = Cars()
     @State private var camera = Camera()
+    @State private var duration: Double = 0
     
     @State private var prevX: Double = 0 // Stores last x coord in gesture to check direction of gesture when next x coord comes in
     @State private var prevY: Double = 0
@@ -37,13 +21,14 @@ struct LapComparisonView: View {
     private let radius: Float = 3 // Radius of rotation of camera around y-axis
     private let xModifier: Float = 0.05 // Scales gesture distance to change in coords of camera
     private let yModifier: Float = 0.0001
-
     
     var body: some View {
         Button("MOVE") {
+            duration = 10
             cars.car1.x += 1
         }
-        ARViewContainer(camera: $camera, cars: $cars)
+        
+        ARViewContainer(camera: $camera, cars: $cars, duration: $duration)
             .gesture(
                 DragGesture()
                     .onChanged { translate in
@@ -91,7 +76,7 @@ struct LapComparisonView: View {
                         }
                         
                         // If zSign is -ve, camera is "behind" the point of reference and camera.z should be negative
-                        camera.z = zSign ? sqrt(powf(radius,2) - powf(camera.x, 2)) : -sqrt(powf(radius,2) - powf(camera.x, 2))
+                        camera.z = zSign ? sqrt(pow(radius,2) - pow(camera.x, 2)) : -sqrt(pow(radius,2) - pow(camera.x, 2))
                     }
             )
     }
@@ -101,10 +86,13 @@ struct ARViewContainer: UIViewRepresentable {
     
     @Binding var camera: Camera
     @Binding var cars: Cars
-
+    @Binding var duration: Double
+    
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero, cameraMode: .nonAR, automaticallyConfigureSession: false)
+        let arView = CustomARView(frame: .zero, camera: camera)
+        arView.cameraMode = .nonAR
+        arView.automaticallyConfigureSession = false
         arView.debugOptions = .showWorldOrigin
 
         let newAnchor = AnchorEntity(world: [0, 0, 0])
@@ -113,17 +101,55 @@ struct ARViewContainer: UIViewRepresentable {
         arView.scene.anchors.append(newAnchor)
         
         let cameraAnchor = AnchorEntity(world: [camera.x, camera.y, camera.z])
-        let camera = PerspectiveCamera()
-        cameraAnchor.addChild(camera)
+        let perspectiveCamera = PerspectiveCamera()
+        cameraAnchor.addChild(perspectiveCamera)
         arView.scene.anchors.append(cameraAnchor)
-                    
+                                            
         return arView
 
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
-        uiView.scene.anchors[0].move(to: Transform(translation: [cars.car1.x, 0, cars.car1.y]), relativeTo: nil, duration: 10)
-        uiView.scene.anchors[1].move(to: Transform(translation: [camera.x, camera.y, camera.z]), relativeTo: uiView.scene.anchors[0])
-        uiView.scene.anchors[1].look(at: uiView.scene.anchors[0].position(relativeTo: nil), from: uiView.scene.anchors[1].position(relativeTo: nil), upVector: [0, 1, 0], relativeTo: nil)
+        uiView.scene.anchors[0].move(to: Transform(translation: [cars.car1.x, 0, cars.car1.y]), relativeTo: nil, duration: duration)
     }
+}
+
+class CustomARView: ARView {
+    
+    private var subscription: AnyCancellable?
+        
+    required init(frame: CGRect, camera: Camera) {
+        super.init(frame: frame)
+        
+        subscription = scene.publisher(for: SceneEvents.Update.self, on: nil).sink(receiveValue: { _ in
+            super.scene.anchors[1].move(to: Transform(translation: [camera.x, camera.y, camera.z]), relativeTo: super.scene.anchors[0])
+            super.scene.anchors[1].look(at: super.scene.anchors[0].position(relativeTo: nil), from: super.scene.anchors[1].position(relativeTo: nil), upVector: [0, 1, 0], relativeTo: nil)
+        })
+    }
+    
+    @MainActor required dynamic init(frame frameRect: CGRect) {
+        fatalError("init(frame:) has not been implemented")
+    }
+
+    @MainActor required dynamic init?(coder decoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Camera {
+    // Stores the x, y and z angles for the camera
+    var x: Float = 0
+    var y: Float = 0.5
+    var z: Float = 3
+}
+
+struct Cars {
+    struct Car {
+        // Stores the x, y coords of cars
+        var x: Float = 0
+        var y: Float = 0
+    }
+    
+    var car1 = Car()
+    var car2 = Car()
 }
